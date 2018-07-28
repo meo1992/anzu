e651fc7d9cc7cfce6f8b779bea57949b3d9d468a
TAIKI <chtbk2001@gmail.com>
2018-07-28 12:19:51 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 
 
 
-module PetsHuggy
+module Anzu
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
